c3532748f031d07651f43d2b916158cc7ed4d80b
Achouffe666 <austin@benard.be>
2021-01-28 12:52:34 +0100

    adding dynamic clock

---
+++ src/clock.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+
+class Clock extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+        this.timer = null;
+    }
+
+    componentDidMount() {
+        this.timer = window.setInterval(this.tick.bind(this), 1000);
+    }
+
+    componentWillUnmount() {
+        window.clearInterval(this.timer);
+    }
+
+    tick() {
+        this.setState({date: new Date()});
+    }
+
+    render() {
+        return (
+            <div>
+                {"it's "}
+                {this.state.date.toLocaleTimeString()}
+            </div>
+        );
+    }
+}
+
+module.exports.Clock = Clock;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 // index.js
 import React from "react";
 import {render} from "react-dom";
+import Clock from "./clock.js";
 
-// eslint-disable-next-line react/jsx-no-literals
-render(<h1>Pomodoro</h1>, document.querySelector("#root"));
+function Welcome({name, age, children}) {
+    return (
+        <div>
+            <h1>
+                {"Bonjour, "}
+                {name}
+            </h1>
+            <h6>
+                {"Vous avez "}
+                {age}
+                {" ans"}
+            </h6>
+            <p>{children}</p>
+            <Clock />
+        </div>
+    );
+}
+render(
+    <Welcome name={"Jean"} age={34}>
+        {" "}
+        {"Bienvenue à vous!"}
+    </Welcome>,
+    document.querySelector("#root"),
+);
+//npx eslint src --fix

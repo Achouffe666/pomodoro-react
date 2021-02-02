730fbbb23f6f4f52918a3a96f518151b94f9d423
Achouffe666 <austin@benard.be>
2021-02-02 16:20:45 +0100

    adding modal

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 // index.js
-import React from "react";
+import React, {useState} from "react";
 import {render} from "react-dom";
 import {Clock} from "./clock.js";
 import Timer from "./timer.js";
+import Modal from "./modal.js";
 
 function Main() {
+    const [isOpen, setIsOpen] = useState(false);
+    const refreshPage = () => {
+        window.location.reload();
+    };
     return (
         <div className={"main"}>
+            <button type={"button"} onClick={() => setIsOpen(true)}>
+                {"Open modal"}
+            </button>
             <Clock />
             <Timer />
+            <Modal
+                reset={refreshPage}
+                open={isOpen}
+                onClose={() => setIsOpen(false)}>
+                {"Timer is finished, what do you want to do now?"}
+            </Modal>
         </div>
     );
 }
 
-render(
-    <Main />,
-
-    document.querySelector("#root"),
-);
+render(<Main />, document.querySelector("#root"));
 
 //npx eslint src --fix
--- src/modal.js
+++ src/modal.js
@@ -1,1 +1,34 @@
-// import React from "react";
+import React from "react";
+import ReactDom from "react-dom";
+
+export default function Modal({open, children, onClose, reset}) {
+    if (!open) {
+        return null;
+    }
+    return ReactDom.createPortal(
+        <>
+            <div className={"overlay"} />
+            <div className={"modal"}>
+                <p>{children}</p>
+                <br />
+                <div className={"container"}>
+                    <button type={"button"} onClick={reset}>
+                        {"Start new!"}
+                    </button>
+                    <button type={"button"} onClick={onClose}>
+                        {"Close modal"}
+                    </button>
+                </div>
+            </div>
+        </>,
+        document.querySelector("#portal"),
+    );
+}
+
+// ne pas mettre 2X le nom
+// je I not i
+// esapcement entre les titres et txt
+// photo ecrasé
+// good to know about me sans :
+// espacements les même partout, titres les memes aussi
+// junior web developer en grand a coté du nom
